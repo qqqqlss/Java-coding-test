ebbab0f248509787f9c78837c3961577d0b70a09
qqqqlss <qqqqlss@naver.com>
2022-11-23 23:16:31 +0900

    solve 거스름돈.java

--- stack_queue/같은숫자는싫어.java
+++ stack_queue/같은숫자는싫어.java
@@ -1,5 +1,26 @@
 package stack_queue;
+import java.util.*;
 
 public class 같은숫자는싫어 {
-    
+    public class Solution {
+        public int[] solution(int []arr) {
+            LinkedList<Integer> answer_list = new LinkedList<>();
+            //System.out.println("Hello Java");
+            int index=arr[0];
+            for (int i : arr){
+                if (i!=index){
+                    answer_list.add(index);
+                    index=i;
+                }
+            }
+            answer_list.add(index);
+            int[] answer= new int[answer_list.size()];
+            index=0;
+            for (int i : answer_list){
+                answer[index]=i;
+                index++;
+            }
+            return answer;
+        }
+    }
 }
---
+++ 고급알고리즘/README.md
@@ -0,0 +1,2 @@
+# Java-coding-test.고급알고리즘
+거스름돈 - greedy
