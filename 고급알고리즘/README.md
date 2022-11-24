01ff0286bc22397c315125b2729bd0b42cce9914
qqqqlss <qqqqlss@naver.com>
2022-11-24 13:52:00 +0900

    solve 배열값중0값을찾아배열뒤로배치시키기.java

--- 고급알고리즘/README.md
+++ 고급알고리즘/README.md
@@ -1,3 +1,5 @@
 # Java-coding-test.고급알고리즘
 거스름돈 - greedy
 곱하기혹은더하기 - greedy, 0~9 문자는 str.charAt(0) - '0' 통해 숫자로 변환
+ATM - greedy, Arrays.sort(), scan.nextInt()는 띄어쓰기로도 구분
+배열값중0값을찾아배열뒤로배치시키기 - 배열 활용 시간,효율성up
---
+++ 고급알고리즘/배열값중0값을찾아배열뒤로배치시키기.java
@@ -0,0 +1,22 @@
+package 고급알고리즘;
+
+import java.util.Arrays;
+public class 배열값중0값을찾아배열뒤로배치시키기 {
+    public static void reorder(int[] arr) {
+        int j = 0;
+        for (int item : arr) {
+            if (item != 0) {
+                arr[j] = item;
+                j++; 
+            }
+        }
+        for (int i = j; i < arr.length; i++) {
+            arr[i] = 0;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = { 6, 0, 8, 2, 3, 0, 4, 0, 1 };
+        reorder(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
