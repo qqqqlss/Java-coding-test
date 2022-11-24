537a7e10ced63b418db53c252313afcf1b424c40
qqqqlss <qqqqlss@naver.com>
2022-11-24 16:37:02 +0900

    solve 타겟넘버.java

---
+++ dfs_bfs/README.md
@@ -0,0 +1,2 @@
+# Java-coding-test.코테고득점kit.bfs,dfs
+타겟넘버 -dfs 원래내가 쓰던 방식  전역변수 안쓰고 return값 더해주는게 생소했음.
---
+++ dfs_bfs/타겟넘버.java
@@ -0,0 +1,18 @@
+package dfs_bfs;
+
+public class 타겟넘버 {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+    }
+}
